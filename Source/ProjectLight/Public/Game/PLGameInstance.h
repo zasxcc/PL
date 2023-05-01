754c1ddbb3d6e48c6efb20dd8c78b58075afc138
Park_In_Hyeok <32037739+zasxcc@users.noreply.github.com>
2023-05-01 14:51:40 +0900

    Add "DemonLord", "SoulCave", GameInstance, Widget

---
+++ Source/ProjectLight/Public/Game/PLGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PLGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTLIGHT_API UPLGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ProjectLight/Public/UI/PLWidgetBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidgetBlueprint.h"
+#include "PLWidgetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTLIGHT_API UPLWidgetBase : public UUserWidgetBlueprint
+{
+	GENERATED_BODY()
+	
+};
