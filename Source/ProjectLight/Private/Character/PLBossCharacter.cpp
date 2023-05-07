7d5dc6d317bcc2a2ab680408dd3f39a60e278f2e
Park_In_Hyeok <32037739+zasxcc@users.noreply.github.com>
2023-05-07 22:55:30 +0900

    Update FlickeringLIght

---
+++ Source/ProjectLight/Private/Character/PLBossCharacter.cpp
@@ -0,0 +1,96 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/PLBossCharacter.h"
+#include "Components/PointLightComponent.h"
+
+APLBossCharacter::APLBossCharacter()
+{
+	FlickeringPointLightComponent = CreateDefaultSubobject<UPointLightComponent>(TEXT("FlickerPointLightComp"));
+	FlickeringPointLightComponent->SetupAttachment(GetMesh(), "FlickerSock");
+}
+
+void APLBossCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void APLBossCharacter::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void APLBossCharacter::DeadEvent()
+{
+	Super::DeadEvent();
+}
+
+void APLBossCharacter::BecomeTargetEvent()
+{
+	//Super::BecomeTargetEvent();
+}
+
+void APLBossCharacter::BecomeNonTargetEvent()
+{
+	//Super::BecomeNonTargetEvent();
+}
+
+void APLBossCharacter::SetFlickeringLight_Flicker(float _time, float _flickerInterval)
+{
+	//이미 타이머가 진행중이면 클리어
+	if(FlickerIntervalTimer.IsValid())
+	{
+		GetWorldTimerManager().ClearTimer(FlickerIntervalTimer);
+	}
+
+	// 포인트 라이트 깜빡임 타이머 셋
+	GetWorld()->GetTimerManager().SetTimer(FlickerIntervalTimer, this, &APLBossCharacter::LightFlicker, _flickerInterval, true);
+
+	// 포인트 라이트 깜빡임 지속시간 타이머 셋
+	GetWorld()->GetTimerManager().SetTimer(FlickerTimer, FTimerDelegate::CreateLambda([&]()
+	{
+		if(FlickerIntervalTimer.IsValid())
+		{
+			GetWorldTimerManager().ClearTimer(FlickerIntervalTimer);
+		}
+	}), _time, false); 
+	
+}
+
+void APLBossCharacter::StopFlickeringPointLight(bool _lightOn)
+{
+	if(FlickerIntervalTimer.IsValid())
+	{
+		GetWorldTimerManager().ClearTimer(FlickerIntervalTimer);
+	}
+	if(FlickerTimer.IsValid())
+	{
+		GetWorldTimerManager().ClearTimer(FlickerTimer);
+	}
+
+	if(_lightOn)
+	{
+		GetFlickerPointLight()->SetLightingChannels(false, true, false);
+		HasFlickerLightChannel2 = true;
+	}
+	else if(_lightOn == false)
+	{
+		GetFlickerPointLight()->SetLightingChannels(false, false, false);
+		HasFlickerLightChannel2 = false;
+	}
+
+}
+
+void APLBossCharacter::LightFlicker()
+{
+	if(HasFlickerLightChannel2)
+	{
+		GetFlickerPointLight()->SetLightingChannels(false, false, false);
+		HasFlickerLightChannel2 = false;
+	}
+	else
+	{
+		GetFlickerPointLight()->SetLightingChannels(false, true, false);
+		HasFlickerLightChannel2 = true;
+	}
+}
---
+++ Source/ProjectLight/Public/Character/PLBossCharacter.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/PLAICharacter.h"
+#include "PLBossCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTLIGHT_API APLBossCharacter : public APLAICharacter
+{
+	GENERATED_BODY()
+
+public:
+	APLBossCharacter();
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+	virtual void DeadEvent() override;
+	virtual void BecomeTargetEvent() override;
+	virtual void BecomeNonTargetEvent() override;
+
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=PL_Character)
+	TObjectPtr<class UPointLightComponent> FlickeringPointLightComponent;
+
+private:
+	UPROPERTY()
+	FTimerHandle FlickerTimer;
+
+	UPROPERTY()
+	FTimerHandle FlickerIntervalTimer;
+
+	bool HasFlickerLightChannel2 = true;
+	
+	
+public:
+	// 포인트 라이트 깜빡임 셋
+	UFUNCTION(BlueprintCallable)
+	void SetFlickeringLight_Flicker(float _time, float _flickerInterval);
+
+	// 포인트 라이트 깜빡임 정지
+	UFUNCTION(BlueprintCallable)
+	void StopFlickeringPointLight(bool _lightOn);
+
+	// 함수 호출 시, HasFlickerLightChannel2에 따라 FlickerPointLightComponent이 켜지거나 꺼지거나 
+	UFUNCTION()
+	void LightFlicker();
+
+	//////////Getter, Setter////////////
+	UFUNCTION(BlueprintPure)
+	class UPointLightComponent* GetFlickerPointLight() const
+	{
+		return FlickeringPointLightComponent;
+	}
+};
